92dea11ebdb72b44d8042fb55ecbb027bfe0870b
plkumjorn <plkumjorn@gmail.com>
2015-04-25 16:59:46 +0700

    edit toolbar

---
+++ js/main_script.js
@@ -0,0 +1,38 @@
+$(function () {
+	$('.dropdown-toggle').dropdown();
+	$('#myTab a:last').tab('show');
+
+	$(".toolbutton").click(function(){
+		if($(this).hasClass('current')){
+			$(this).removeClass('current');
+		}
+		else{
+			$(".toolbutton").removeClass('current');
+			$(this).addClass('current');
+		}
+	});
+
+	$(".aClass").click(function(){
+		var changedText = $(this).text()+' ';
+		var colorPoint = $('<i class="fa fa-circle" style="color:red;"></i>');
+		if($(this).hasClass('CB')){
+			colorPoint = $('<i class="fa fa-circle" style="color:green;"></i>');
+		}
+		else if($(this).hasClass('CC')){
+			colorPoint = $('<i class="fa fa-circle" style="color:blue;"></i>');
+		}
+		else if($(this).hasClass('CD')){
+			colorPoint = $('<i class="fa fa-circle" style="color:orange;"></i>');
+		}
+		else if($(this).hasClass('CE')){
+			colorPoint = $('<i class="fa fa-circle" style="color:purple;"></i>');
+		}
+		else if($(this).hasClass('CU')){
+			colorPoint = $('<i class="fa fa-circle" style="color:grey;"></i>');
+		}
+		$("#dropdownMenu2").text(changedText);
+		$("#dropdownMenu2").prepend(colorPoint);
+		var drop = $('<span class="caret"></span>');
+		$("#dropdownMenu2").append(drop);
+	});
+})
